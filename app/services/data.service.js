ceb24cd8de72ebf5fe0f7db61a175cf9aefc5501
Jono Wilkinson <jw@jettylabs.com>
2019-02-10 20:38:39 +1100

    Adds data service with methods to 'getAllProducts' and 'getRecommendedProducts'

---
+++ app/services/data.service.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const requestPromise = require('request-promise-native');
+
+const config = require('../resources/config.json');
+const Product = require('../models/Product.model.js');
+
+const service = {};
+
+service.getAllProducts = async () => {
+    const products = [];
+
+    const requestOpts = {
+        method: 'GET',
+        url: `${config.dataServiceBaseUrl}/products`,
+        qs: {
+            token: config.token
+        },
+        json: true
+    };
+    const rawProductData = await requestPromise(requestOpts);
+
+    // Convert retrieved data to local Product model instances
+    rawProductData.forEach(rawProduct => {
+        products.push( new Product(rawProduct) );
+    });
+
+    return products;
+};
+
+service.getRecommendedProducts = async () => {
+    const products = [];
+
+    const requestOpts = {
+        method: 'GET',
+        url: `${config.dataServiceBaseUrl}/shopperHistory`,
+        qs: {
+            token: config.token
+        },
+        json: true
+    };
+
+    const rawShopperHistoryData = await requestPromise(requestOpts);
+
+    // get the distinct products with aggregated quantities
+    const allProductsWithDuplicates = rawShopperHistoryData.reduce((accumulate, customer) => {
+        return accumulate.concat(customer.products);
+    }, []);
+
+    const aggregatedProducts = allProductsWithDuplicates.reduce(function (distinctProducts, product) {
+        if (distinctProducts[product.name]) {
+            distinctProducts[product.name].quantity += product.quantity;
+        } else {
+            distinctProducts[product.name] = product;
+        }
+        return distinctProducts;
+    }, {});
+
+    // Convert retrieved data to local Product model instances
+    Object.values(aggregatedProducts).forEach(rawProduct => {
+        products.push( new Product(rawProduct) );
+    });
+
+    return products;
+}
+
+module.exports = service;
